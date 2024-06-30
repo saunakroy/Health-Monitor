5d77a373a32206d2cf66184a1673686ef80258a1
Saunak Roy <saunakroy@Saunaks-M2-MacBook-Air.local>
2024-06-30 13:37:05 -0400

    Initial commit

---
+++ openai-test.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "I'm experiencing a headache, a runny nose, and my body feels sensitive all over. What illness could I have and how could I remedy it?"}
+  ]
+)
+
+print(completion.choices[0].message)
