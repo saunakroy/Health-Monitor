9d3837a7b6a166c603ac95703a89f0ee6815cba0
Joshua Chen <josheewa7@gmail.com>
2024-08-08 12:19:36 +0800

    Create base layout
    Created sidebar, main page, set up basic layout

---
+++ components/Navigation/Sidebar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import Link from 'next/link'
+
+const Sidebar = ({ children }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen)
+  }
+
+  return (
+    <div className="h-screen w-full flex fixed">
+      <div className="sidebar h-screen w-96 flex bg-gray-900 text-white flex-col justify-between">
+        <div className="p-5 border-y border-gray-300 bg-gray-200">
+          <div className="flex items-center cursor-pointer text-2xl text-black" onClick={toggleDropdown}>
+            <img src={'/favicon.ico'} alt="Profile Picture" className="w-10 h-10 rounded-full mr-2" />
+            <span>James Paul Turner</span>
+            <div
+              className={`absolute top-16 left-5 bg-white border border-gray-300 shadow-lg ${
+                dropdownOpen ? 'flex' : 'hidden'
+              } flex-col`}>
+              <div className="p-2">
+                <p>Profile</p>
+                <p>Settings</p>
+                <p>Help</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <nav className="flex-grow flex flex-col text-center text-xl">
+          <Link href="/analytics" className="sidebar-items">
+            Analytics
+          </Link>
+          <Link href="/diagnose" className="sidebar-items">
+            Diagnose
+          </Link>
+          <Link href="/image-analysis" className="sidebar-items">
+            Image Analysis
+          </Link>
+          <Link href="/history" className="sidebar-items">
+            History
+          </Link>
+        </nav>
+        <div className="p-5 border-t border-gray-300">
+          <button className="w-full py-2 bg-red-600 text-white hover:bg-red-700">Logout</button>
+        </div>
+      </div>
+      <div className="flex-1">{children}</div>
+    </div>
+  )
+}
+
+export default Sidebar
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
-
+import Head from 'next/head'
+import '@/styles/globals.css'
+import Sidebar from '@/components/Navigation/Sidebar'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Harmony</title>
+      </Head>
+      <Sidebar>
+        <Component {...pageProps} />
+      </Sidebar>
+    </>
+  )
 }
