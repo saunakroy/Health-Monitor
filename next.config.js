be6d6b2095782a8a2a6baf6c6debb33ca462219e
Joshua Chen <josheewa7@gmail.com>
2024-09-04 17:45:02 -0400

    Set up basic flask test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
       },
     ]
